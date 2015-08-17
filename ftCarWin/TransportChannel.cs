db320ad31fd30ea879fdd5b10b444f1295ad1329
pimsturm <pim.sturm@gmail.com>
2015-08-18 00:00:30 +0200

    User settings

---
+++ ftCarWin/TransportChannel.cs
@@ -0,0 +1,15 @@
+﻿namespace ftCarWin {
+    /// <summary>
+    /// The transport channel used for communicating with the Arduino.
+    /// </summary>
+    public enum TransportChannel {
+        /// <summary>
+        /// Serial port
+        /// </summary>
+        SerialPort, 
+        /// <summary>
+        /// BlueTooth
+        /// </summary>
+        BlueTooth,
+    }
+}
